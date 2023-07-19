ed536ba1fad21b9a42d689621d87607b617359ec
lakshmidusanapudi <85738180+lakshmidusanapudi@users.noreply.github.com>
2023-07-19 15:32:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bulboff()
+{
+    document.getElementById("bulb").src="https://freepngimg.com/save/31669-bulb-off-transparent-image/500x500";
+}
+function bulbon()
+{
+    document.getElementById("bulb").src="https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg?w=2000";
+}
